8794eaeb95d77bc169778c078dd4446766e70721
Lucas <lucas.hatron@gmail.com>
2023-02-20 14:54:31 +0000

    Débboguer

--- guestbook/src/Controller/ConferenceController.php
+++ guestbook/src/Controller/ConferenceController.php
@@ -3,16 +3,21 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ConferenceController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        return $this->render('conference/index.html.twig', [
-            'controller_name' => 'ConferenceController',
-        ]);
+        dd($request);
+
+        return new Response(<<<EOF
+            <html>
+                <body><img src="/images/under-construction.gif" /></body>
+            </html>
+        EOF);
     }
 }
